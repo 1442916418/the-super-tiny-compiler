68db18cd34f2160022532571f4e325f4bff7be81
shuisheng <1442916418@qq.com>
2023-03-24 17:15:52 +0800

    test...

--- test.js
+++ test.js
@@ -1,84 +1,100 @@
-const {
-  tokenizer,
-  parser,
-  transformer,
-  codeGenerator,
-  compiler,
-} = require('./the-super-tiny-compiler');
-const assert = require('assert');
+const { tokenizer, parser, transformer, codeGenerator, compiler } = require('./the-super-tiny-compiler-zh')
+const assert = require('assert')
 
-const input  = '(add 2 (subtract 4 2))';
-const output = 'add(2, subtract(4, 2));';
+const input = '(add 2 (subtract 4 2))'
+const output = 'add(2, subtract(4, 2));'
 
 const tokens = [
-  { type: 'paren',  value: '('        },
-  { type: 'name',   value: 'add'      },
-  { type: 'number', value: '2'        },
-  { type: 'paren',  value: '('        },
-  { type: 'name',   value: 'subtract' },
-  { type: 'number', value: '4'        },
-  { type: 'number', value: '2'        },
-  { type: 'paren',  value: ')'        },
-  { type: 'paren',  value: ')'        }
-];
+  { type: 'paren', value: '(' },
+  { type: 'name', value: 'add' },
+  { type: 'number', value: '2' },
+  { type: 'paren', value: '(' },
+  { type: 'name', value: 'subtract' },
+  { type: 'number', value: '4' },
+  { type: 'number', value: '2' },
+  { type: 'paren', value: ')' },
+  { type: 'paren', value: ')' }
+]
 
 const ast = {
   type: 'Program',
-  body: [{
-    type: 'CallExpression',
-    name: 'add',
-    params: [{
-      type: 'NumberLiteral',
-      value: '2'
-    }, {
+  body: [
+    {
       type: 'CallExpression',
-      name: 'subtract',
-      params: [{
-        type: 'NumberLiteral',
-        value: '4'
-      }, {
-        type: 'NumberLiteral',
-        value: '2'
-      }]
-    }]
-  }]
-};
+      name: 'add',
+      params: [
+        {
+          type: 'NumberLiteral',
+          value: '2'
+        },
+        {
+          type: 'CallExpression',
+          name: 'subtract',
+          params: [
+            {
+              type: 'NumberLiteral',
+              value: '4'
+            },
+            {
+              type: 'NumberLiteral',
+              value: '2'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
 
 const newAst = {
   type: 'Program',
-  body: [{
-    type: 'ExpressionStatement',
-    expression: {
-      type: 'CallExpression',
-      callee: {
-        type: 'Identifier',
-        name: 'add'
-      },
-      arguments: [{
-        type: 'NumberLiteral',
-        value: '2'
-      }, {
+  body: [
+    {
+      type: 'ExpressionStatement',
+      expression: {
         type: 'CallExpression',
         callee: {
           type: 'Identifier',
-          name: 'subtract'
+          name: 'add'
         },
-        arguments: [{
-          type: 'NumberLiteral',
-          value: '4'
-        }, {
-          type: 'NumberLiteral',
-          value: '2'
-        }]
-      }]
+        arguments: [
+          {
+            type: 'NumberLiteral',
+            value: '2'
+          },
+          {
+            type: 'CallExpression',
+            callee: {
+              type: 'Identifier',
+              name: 'subtract'
+            },
+            arguments: [
+              {
+                type: 'NumberLiteral',
+                value: '4'
+              },
+              {
+                type: 'NumberLiteral',
+                value: '2'
+              }
+            ]
+          }
+        ]
+      }
     }
-  }]
-};
+  ]
+}
 
-assert.deepStrictEqual(tokenizer(input), tokens, 'Tokenizer should turn `input` string into `tokens` array');
-assert.deepStrictEqual(parser(tokens), ast, 'Parser should turn `tokens` array into `ast`');
-assert.deepStrictEqual(transformer(ast), newAst, 'Transformer should turn `ast` into a `newAst`');
-assert.deepStrictEqual(codeGenerator(newAst), output, 'Code Generator should turn `newAst` into `output` string');
-assert.deepStrictEqual(compiler(input), output, 'Compiler should turn `input` into `output`');
+// tokenizer(input)
+parser(tokens)
+// transformer(ast)
+// codeGenerator(newAst)
+// compiler(input)
 
-console.log('All Passed!');
+// assert.deepStrictEqual(tokenizer(input), tokens, 'Tokenizer 应将 `input` 字符串转换为 `tokens` 数组')
+// assert.deepStrictEqual(parser(tokens), ast, '解析器应将 `tokens` 数组转换为 `ast` ')
+// assert.deepStrictEqual(transformer(ast), newAst, 'Transformer 应该将 `ast` 变成 `newAst`')
+// assert.deepStrictEqual(codeGenerator(newAst), output, '代码生成器应该将 `newAst` 转换为 `output` 字符串')
+// assert.deepStrictEqual(compiler(input), output, '编译器应将 `输入` 转换为 `输出` ')
+
+// console.log('All Passed!')
